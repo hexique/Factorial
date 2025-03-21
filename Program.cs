94134fd48d86c12e572f94ff8f566c1503423a64
hexique <69116911+hexique@users.noreply.github.com>
2025-03-21 21:36:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿while (true)
+{
+    long counter = 1;
+    long result = 1;
+    Console.WriteLine("Введите число:");
+    long number = long.Parse(Console.ReadLine());
+    while (counter != number)
+    {
+        counter += 1;
+        result *= counter;
+        if (counter == number)
+        {
+            Console.WriteLine($"Результат: {result}");
+            break;
+        }
+    }
+}
